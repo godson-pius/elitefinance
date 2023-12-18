93af6ca63563906c289aaf056131b6c8f055283b
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2023-12-18 14:28:43 +0100

    first commit

---
+++ components/mobile-nav.php
@@ -0,0 +1,30 @@
+<div class="mobile-nav__wrapper">
+    <div class="mobile-nav__overlay mobile-nav__toggler"></div>
+    <div class="mobile-nav__content">
+        <span class="mobile-nav__close mobile-nav__toggler">
+            <i class="fas fa-plus"></i>
+        </span>
+        <div class="logo-box">
+            <a href="index" aria-label="logo image">
+                <img src="assets/images/resources/logo-1.png" alt="" />
+            </a>
+        </div>
+        <div class="mobile-nav__container"></div>
+        <ul class="mobile-nav__contact list-unstyled">
+            <li>
+                <i class="fa fa-envelope"></i>
+                <a href="mailto:info@example.com">info@elitefinance.com</a>
+            </li>
+            <li>
+                <i class="fa fa-phone-alt"></i>
+                <a href="tel:123456789">(435) 932-0111</a>
+            </li>
+        </ul>
+        <div class="mobile-nav__social">
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-facebook-square"></a>
+            <a href="#" class="fab fa-pinterest-p"></a>
+            <a href="#" class="fab fa-instagram"></a>
+        </div>
+    </div>
+</div>
